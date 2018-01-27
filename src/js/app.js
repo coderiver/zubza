29d07a5828c15725f2480a4fc9b14fa10bcad8ab
yana-coderiver <yana@coderiver.com.ua>
2018-01-27 18:32:13 +0200

    Clinic gallery

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import sayHello from './lib/sayHello.js';
 import faqDrop from './components/faqDrop.js';
 import sliders from './components/sliders.js';
 import './components/select.js';
+import './components/gallery.js';
 
 sayHello();
 $(document).ready(function() {
