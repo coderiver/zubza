792635f618235690506f3fd62065ccf0b191ddc8
maks <maks@coderiver.com.ua>
2018-01-27 15:27:46 +0200

    hero

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import sayHello from './lib/sayHello.js';
 import faqDrop from './components/faqDrop.js';
 import sliders from './components/sliders.js';
+import './components/select.js';
 
 sayHello();
 $(document).ready(function() {
