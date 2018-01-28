575437f0bb75ae0427366c8faf2854fcfec58c93
Богдан Кононов <b.b.kononov@gmail.com>
2018-01-28 13:20:24 +0200

    upd

--- src/js/components/burgerMenu.js
+++ src/js/components/burgerMenu.js
@@ -1,8 +1,10 @@
 import {DOC, ACTIVE} from '../_const';
 
 const burger = '.js-burger';
+const submenu = $('.js-submenu');
 
 DOC.on('click', burger, (e) => {
   let that = $(e.currentTarget);
   that.toggleClass(ACTIVE);
+  submenu.toggleClass(ACTIVE);
 });
