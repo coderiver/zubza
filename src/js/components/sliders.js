d55355fd1c1803fcf0dcee109ee6f91f30e79c25
Olha Yevtushenko <olya@coderiver.com.ua>
2018-01-28 22:27:14 +0200

    add rel posts mobile

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -11,6 +11,19 @@ function sliders() {
   <polygon points="0,19.4 20.7,0 192,160 20.7,320 0,300.7 150.5,160 "/>
   </svg></i></div>`;
 
+  let postBtnPrev = `<button class="btn-prev js-post-prev">
+  <svg width="16" height="19" xmlns="http://www.w3.org/2000/svg">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 19">
+  <path d="M16 19L0 9.6 16 0c-.6 3.2-1 6.4-1 9.6 0 3.1.4 6.3 1 9.4z" fill="#ffffff"/>
+  </svg></svg></button>`;
+  let postBtnNext = `<button class="btn-next js-post-next">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 19">
+  <path d="M0 0l16 9.4L0 19c.6-3.2 1-6.4 1-9.6C1 6.3.6 3.1 0 0z" fill="#ffffff"/>
+  </svg></button>`;
+
+
+
+
   let sliderWithCount = $('.js-slider-count');
   sliderWithCount.each(function(index, elem) {
 
@@ -27,7 +40,7 @@ function sliders() {
     $this.slick({
       infinite: false,
       slidesToShow: 1,
-      slidesToScroll: 1, 
+      slidesToScroll: 1,
       prevArrow: btnPrev,
       nextArrow: btnNext
     });
@@ -40,7 +53,7 @@ function sliders() {
   $('.js-dots-slider-oneslide').slick({
     infinite: false,
     slidesToShow: 1,
-    slidesToScroll: 1, 
+    slidesToScroll: 1,
     prevArrow: btnPrev,
     nextArrow: btnNext,
     dots: true
@@ -48,7 +61,7 @@ function sliders() {
   $('.js-double-slider').slick({
     infinite: false,
     slidesToShow: 2,
-    slidesToScroll: 1, 
+    slidesToScroll: 1,
     prevArrow: btnPrev,
     nextArrow: btnNext,
     dots: false
@@ -62,6 +75,49 @@ function sliders() {
     slidesToScroll: 1,
     prevArrow: $('.js-badge-prev'),
     nextArrow: $('.js-badge-next')
+  });
+
+
+  $('.js-mob-posts-slider').slick({
+    responsive: [
+      {
+        breakpoint: 4000,
+        settings: 'unslick'
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          infinite: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          prevArrow: postBtnPrev,
+          nextArrow: postBtnNext,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          infinite: false,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          prevArrow: postBtnPrev,
+          nextArrow: postBtnNext,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 599,
+        settings: {
+          infinite: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          prevArrow: postBtnPrev,
+          nextArrow: postBtnNext,
+          dots: true
+        }
+      }
+    ]
   });
 
 };
