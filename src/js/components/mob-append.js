b02c927de325b446cb84991759f1dd79124f8634
Olha Yevtushenko <olya@coderiver.com.ua>
2018-01-29 19:07:51 +0200

    clinic boxes mobile

--- src/js/components/mob-append.js
+++ src/js/components/mob-append.js
@@ -1,25 +1,39 @@
 export default function mobAppend() {
-  $('.js-mob-move').each(function() {
-    let element = $(this);
-    let target = '.' + $(this).data('target');
-    let source = '.' + $(this).data('source');
 
-    console.log(target);
 
-    function appendElements(elem) {
-      if (window.matchMedia('(max-width: 1023px)').matches) {
-        $(elem).appendTo(target);
-      }
-      if (window.matchMedia('(min-width: 1024px)').matches) {
-        $(elem).appendTo(source);
-      }
+  function appendElements() {
+    if (window.matchMedia('(max-width: 1023px)').matches) {
+      $('.js-map-move').appendTo('.js-map-mob-append');
+
+      $('.js-right-move').each(function() {
+        let mob = $(this).closest('.clinic-box').find('.js-right-mob-append');
+        $(this).appendTo(mob);
+      });
+
+      $('.js-title-move').each(function() {
+        let mob = $(this).closest('.clinic-box').find('.js-title-mob-prepend');
+        $(this).prependTo(mob);
+      });
     }
+    if (window.matchMedia('(min-width: 1024px)').matches) {
+      $('.js-map-move').appendTo('.js-map-desk-append');
 
-    appendElements(element);
+      $('.js-right-move').each(function() {
+        let desk = $(this).closest('.clinic-box');
+        $(this).appendTo(desk);
+      });
 
-    $(window).on('resize', function(e) {
-      appendElements(element);
-    });
+      $('.js-title-move').each(function() {
+        let desk = $(this).closest('.clinic-box').find('.js-title-desk-prepend');
+        $(this).prependTo(desk);
+      });
+    }
+  }
+
+  appendElements();
+
+  $(window).on('resize', function(e) {
+    appendElements();
   });
 }
 
