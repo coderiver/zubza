1fbe95d6a6e0026aa2553c393b1e43ec959ccf4f
Olha Yevtushenko <olya@coderiver.com.ua>
2018-02-05 09:07:50 +0200

    Add popup links to blocks

--- src/js/components/mob-append.js
+++ src/js/components/mob-append.js
@@ -4,29 +4,9 @@ export default function mobAppend() {
   function appendElements() {
     if (window.matchMedia('(max-width: 1023px)').matches) {
       $('.js-map-move').appendTo('.js-map-mob-append');
-
-      $('.js-right-move').each(function() {
-        let mob = $(this).closest('.clinic-box').find('.js-right-mob-append');
-        $(this).appendTo(mob);
-      });
-
-      $('.js-title-move').each(function() {
-        let mob = $(this).closest('.clinic-box').find('.js-title-mob-prepend');
-        $(this).prependTo(mob);
-      });
     }
     if (window.matchMedia('(min-width: 1024px)').matches) {
       $('.js-map-move').appendTo('.js-map-desk-append');
-
-      $('.js-right-move').each(function() {
-        let desk = $(this).closest('.clinic-box');
-        $(this).appendTo(desk);
-      });
-
-      $('.js-title-move').each(function() {
-        let desk = $(this).closest('.clinic-box').find('.js-title-desk-prepend');
-        $(this).prependTo(desk);
-      });
     }
   }
 
