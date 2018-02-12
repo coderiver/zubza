6c38c0c2f3c4d79e3ed4e2b40f15667b0d056363
yana-coderiver <yana@coderiver.com.ua>
2018-02-12 18:00:55 +0200

    Filters for mb

--- src/js/components/mob-filters.js
+++ src/js/components/mob-filters.js
@@ -8,6 +8,22 @@ export default function mobFilters() {
     $('.js-mob-filters').removeClass('is-active');
     $('body').removeClass('is-filters-open');
   });
+
+  $('.js-show-meta-filters').on('click', (e) => {
+
+  	const target = e.target || e.srcElement;
+  	const container = $('.js-show-meta-filters');
+  	if(!$(target).hasClass('js-show-meta-item')) return;
+
+  	const select = $(container).find('select');
+  	select.val(`${($(target).text()).trim()}`);
+
+  	$('.js-show-meta-item').each((index, item) => {
+  		$(item).removeClass('is-active');
+  	});
+  	$(target).addClass('is-active');
+  });
+
 }
 
 mobFilters();
